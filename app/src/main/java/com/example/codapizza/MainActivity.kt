e12859221d068e04a4390d75641aa2d7e4731552
Hommaster <ilya.pan.2017@mail.ru>
2024-04-07 22:57:47 +0300

    yeep

--- app/src/main/java/com/example/codapizza/MainActivity.kt
+++ app/src/main/java/com/example/codapizza/MainActivity.kt
@@ -2,17 +2,18 @@ package com.example.codapizza
 
 import android.os.Bundle
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material.Text
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Text(text = "Hellow word!")
+            Column {
+                Text(text = "pineapple")
+                Text(text = "whole pizza")
+            }
         }
     }
 }
