53017a74dc1c3156518c59014c14b23fecaeb640
Hommaster <ilya.pan.2017@mail.ru>
2024-04-11 23:36:23 +0300

    cleaner code

--- app/src/main/java/com/example/codapizza/ui/ToppingCell.kt
+++ app/src/main/java/com/example/codapizza/ui/ToppingCell.kt
@@ -1,6 +1,5 @@
 package com.example.codapizza.ui
 
-import androidx.compose.animation.animateColorAsState
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -9,15 +8,8 @@ import androidx.compose.material.Checkbox
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.drawBehind
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -52,7 +44,7 @@ fun ToppingCell(
     Row (
         verticalAlignment = Alignment.CenterVertically,
         modifier = modifier
-            .clickable { onClickTopping }
+            .clickable { onClickTopping() }
             .padding(horizontal = 16.dp, vertical = 4.dp)
     ) {
         Checkbox(
