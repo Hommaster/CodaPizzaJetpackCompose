899a9aac5a795e87e755f8c6fec552fe0dc4100e
Hommaster <ilya.pan.2017@mail.ru>
2024-04-11 12:35:46 +0300

    update UI (create shape in button PlaceOrder, made a changing background color)

--- app/src/main/java/com/example/codapizza/ui/PizzaBuilderScreen.kt
+++ app/src/main/java/com/example/codapizza/ui/PizzaBuilderScreen.kt
@@ -1,31 +1,63 @@
 package com.example.codapizza.ui
 
-import android.transition.Scene
+import androidx.compose.animation.animateColor
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.LocalTextStyle
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.TransformOrigin
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.intl.Locale
+import androidx.compose.ui.text.style.TextMotion
 import androidx.compose.ui.text.toUpperCase
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.codapizza.R
+import com.example.codapizza.model.Pizza
 import com.example.codapizza.model.Topping
 import com.example.codapizza.model.ToppingPlacement
+
 
 @Preview
 @Composable
 fun PizzaBuilderScreen(
     modifier: Modifier = Modifier
 ) {
+    val infiniteTransition = rememberInfiniteTransition(label = "infinite")
+    val color by infiniteTransition.animateColor(
+        initialValue = Color.White,
+        targetValue = Color.Cyan,
+        animationSpec = infiniteRepeatable(
+            animation = tween(20000, easing = LinearEasing),
+            repeatMode = RepeatMode.Reverse
+        ),
+        label = "color"
+    )
     Column(
         modifier = modifier
+            .drawBehind {
+                drawRect(color)
+            }
     ) {
         ToppingList(
             modifier = modifier
@@ -35,7 +67,7 @@ fun PizzaBuilderScreen(
         OrderButton(
             modifier = modifier
                 .fillMaxWidth()
-                .padding(16.dp)
+                .padding(10.dp)
         )
     }
 }
@@ -50,7 +82,7 @@ private fun ToppingList(
         items(Topping.values()) {toppings ->
             ToppingCell(
                 topping = toppings,
-                placement = ToppingPlacement.Left,
+                placement = ToppingPlacement.All,
                 onClickTopping = {},
                 modifier = modifier
             )
@@ -62,12 +94,33 @@ private fun ToppingList(
 private fun OrderButton(
     modifier: Modifier = Modifier
 ) {
+    val infiniteTransition = rememberInfiniteTransition(label = "infinite transition")
+    val scale by infiniteTransition.animateFloat(
+        initialValue = 1f,
+        targetValue = 1.17f,
+        animationSpec = infiniteRepeatable(tween(10000), RepeatMode.Reverse),
+        label = "scale"
+    )
+
     Button(
         modifier = modifier,
-        onClick = { /*TODO*/ }
+        onClick = { /*TODO*/ },
+        shape = RoundedCornerShape(23.dp),
+        border = BorderStroke(3.dp, Color.Yellow),
+        colors = ButtonDefaults.buttonColors(contentColor = Color.White, backgroundColor = Color.Blue)
     ) {
-        Text(text = stringResource(id = R.string.place_order_button)
-            .toUpperCase(Locale.current)
+        Text(
+            modifier = modifier
+                .padding(135.dp, 0.dp)
+                .graphicsLayer {
+                    scaleX = scale
+                    scaleY = scale
+                    transformOrigin = TransformOrigin.Center
+                }
+                .align(Alignment.Top),
+            style = LocalTextStyle.current.copy(textMotion = TextMotion.Animated),
+            text = stringResource(id = R.string.place_order_button)
+            .toUpperCase(Locale.current),
         )
     }
 }
--- app/src/main/java/com/example/codapizza/ui/ToppingCell.kt
+++ app/src/main/java/com/example/codapizza/ui/ToppingCell.kt
@@ -1,6 +1,6 @@
 package com.example.codapizza.ui
 
-import androidx.compose.foundation.background
+import androidx.compose.animation.animateColorAsState
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -9,8 +9,14 @@ import androidx.compose.material.Checkbox
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
