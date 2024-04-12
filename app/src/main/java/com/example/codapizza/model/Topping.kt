43b893531c2b50079cf108ac2c56bd7dc9aa55c2
Hommaster <ilya.pan.2017@mail.ru>
2024-04-12 22:09:45 +0300

    create toppingDrawable as path to drawable resource of topping

--- app/src/main/java/com/example/codapizza/model/Topping.kt
+++ app/src/main/java/com/example/codapizza/model/Topping.kt
@@ -1,27 +1,35 @@
 package com.example.codapizza.model
 
+import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
 import com.example.codapizza.R
 
 enum class Topping (
-    @StringRes val toppingName: Int
+    @StringRes val toppingName: Int,
+    @DrawableRes val toppingDrawable: Int
 ) {
     Basil(
-        toppingName = R.string.topping_basil
+        toppingName = R.string.topping_basil,
+        toppingDrawable = R.drawable.basil
     ),
     Mushroom(
-        toppingName = R.string.topping_mushroom
+        toppingName = R.string.topping_mushroom,
+        toppingDrawable = R.drawable.mushroom
     ),
     Olives(
-        toppingName = R.string.topping_olives
+        toppingName = R.string.topping_olives,
+        toppingDrawable = R.drawable.olives
     ),
     Peppers(
-        toppingName = R.string.topping_peppers
+        toppingName = R.string.topping_peppers,
+        toppingDrawable = R.drawable.peppers
     ),
     Pepperoni(
-        toppingName = R.string.topping_pepperoni
+        toppingName = R.string.topping_pepperoni,
+        toppingDrawable = R.drawable.pepperoni
     ),
     Pineapple(
-        toppingName = R.string.topping_pineapple
+        toppingName = R.string.topping_pineapple,
+        toppingDrawable = R.drawable.pineapple
     ),
 }
