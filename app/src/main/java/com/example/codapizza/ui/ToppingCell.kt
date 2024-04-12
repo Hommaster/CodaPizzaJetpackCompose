eb031b9c71a2df4382b44d7925f1196ac964568e
Hommaster <ilya.pan.2017@mail.ru>
2024-04-12 22:12:13 +0300

    instead of CheckBox create custom Box for create (i'm wanna round CheckBox)

--- app/src/main/java/com/example/codapizza/ui/ToppingCell.kt
+++ app/src/main/java/com/example/codapizza/ui/ToppingCell.kt
@@ -1,18 +1,40 @@
 package com.example.codapizza.ui
 
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.expandHorizontally
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.slideInHorizontally
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Checkbox
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Check
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.codapizza.R
 import com.example.codapizza.model.Topping
 import com.example.codapizza.model.ToppingPlacement
 
@@ -23,6 +45,7 @@ private fun ToppingCellPreview() {
     ToppingCell(
         topping = Topping.Pepperoni,
         placement = ToppingPlacement.Left,
+        isChecked = true,
         onClickTopping = {}
     )
 }
@@ -31,6 +54,7 @@ private fun ToppingCellPreview() {
 @Composable
 private fun ToppingCallPreviewNotOnPizza() {
     ToppingCell(topping = Topping.Basil, placement = null,
+        isChecked = false,
         onClickTopping = {})
 }
 
@@ -39,27 +63,58 @@ fun ToppingCell(
     topping: Topping,
     placement: ToppingPlacement?,
     modifier: Modifier = Modifier,
+    isChecked: Boolean,
+    size: Float = 24f,
+    checkedColor: Color = Color.Blue,
+    uncheckedColor: Color = Color.White,
     onClickTopping: () -> Unit
 ) {
+
+    val checkboxColor: Color by animateColorAsState(if (isChecked) checkedColor else uncheckedColor)
+    val density = LocalDensity.current
+    val duration = 1000
+
     Row (
         verticalAlignment = Alignment.CenterVertically,
         modifier = modifier
             .clickable { onClickTopping() }
             .padding(horizontal = 16.dp, vertical = 4.dp)
     ) {
-        Checkbox(
-            checked = (placement != null),
-            onCheckedChange = {
-                onClickTopping()
+        Box(
+            modifier = Modifier
+                .size(size.dp)
+                .background(color = checkboxColor, shape = RoundedCornerShape(20.dp))
+                .clickable { onClickTopping() }
+                .border(width = 2.dp, color = Color.Yellow, shape = RoundedCornerShape(20.dp)),
+            contentAlignment = Alignment.Center
+        ) {
+            androidx.compose.animation.AnimatedVisibility(
+                visible = isChecked,
+                enter = slideInHorizontally(animationSpec = tween(duration)) {
+                    with(density) { (size * -0.5).dp.roundToPx() }
+                } + expandHorizontally(
+                    expandFrom = Alignment.Start,
+                    animationSpec = tween(duration)
+                ),
+                exit = fadeOut()
+            ) {
+                Icon(
+                    Icons.Default.Check,
+                    contentDescription = null,
+                    tint = uncheckedColor
+                )
             }
-        )
+        }
         Column (
-            modifier = Modifier.weight(1f, fill = true)
+            modifier = Modifier
+                .weight(1f, fill = true)
                 .padding(start = 4.dp)
 
         ) {
             Text(
                 text = stringResource(topping.toppingName),
+                fontStyle = FontStyle.Italic,
+                fontSize = 20.sp,
                 style = MaterialTheme.typography.body1
             )
             if (placement != null) {
@@ -69,5 +124,11 @@ fun ToppingCell(
                 )
             }
         }
+        Image(
+//            painter = painterResource(id = drawableId),
+//            contentDescription = nameDrawableIcon
+            painterResource(topping.toppingDrawable),
+            "drawableIcon"
+        )
     }
 }
