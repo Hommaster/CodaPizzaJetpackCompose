f3c7d0f944780ff0bf646c4653813c2633406613
Hommaster <ilya.pan.2017@mail.ru>
2024-04-15 14:04:56 +0300

    more clear code

--- app/src/main/java/com/example/codapizza/ui/ToppingCell.kt
+++ app/src/main/java/com/example/codapizza/ui/ToppingCell.kt
@@ -22,11 +22,9 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Check
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -34,7 +32,6 @@ import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.codapizza.R
 import com.example.codapizza.model.Topping
 import com.example.codapizza.model.ToppingPlacement
 
