8f16157a59fa01b5968080f9c51f321edc54f52f
Hommaster <ilya.pan.2017@mail.ru>
2024-04-16 23:49:38 +0300

    new label - None

--- app/src/main/java/com/example/codapizza/model/ToppingPlacement.kt
+++ app/src/main/java/com/example/codapizza/model/ToppingPlacement.kt
@@ -8,5 +8,6 @@ enum class ToppingPlacement (
 ) {
     Left(R.string.placement_left),
     Right(R.string.placement_right),
-    All(R.string.placement_whole)
+    All(R.string.placement_whole),
+    None(R.string.placement_none)
 }
