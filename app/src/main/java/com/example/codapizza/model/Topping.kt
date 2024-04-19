3e7e0c72a5449e97f155278a8aa3dbd98e99b86b
Hommaster <ilya.pan.2017@mail.ru>
2024-04-19 23:39:12 +0300

    created a drawable resources toppings for adding images of toppings on Pizza

--- app/src/main/java/com/example/codapizza/model/Topping.kt
+++ app/src/main/java/com/example/codapizza/model/Topping.kt
@@ -6,30 +6,37 @@ import com.example.codapizza.R
 
 enum class Topping (
     @StringRes val toppingName: Int,
-    @DrawableRes val toppingDrawable: Int
+    @DrawableRes val toppingDrawable: Int,
+    @DrawableRes val pizzaOverlayImage: Int,
 ) {
     Basil(
         toppingName = R.string.topping_basil,
-        toppingDrawable = R.drawable.basil
+        toppingDrawable = R.drawable.basil,
+        pizzaOverlayImage = R.drawable.topping_basil
     ),
     Mushroom(
         toppingName = R.string.topping_mushroom,
-        toppingDrawable = R.drawable.mushroom
+        toppingDrawable = R.drawable.mushroom,
+        pizzaOverlayImage = R.drawable.topping_mushroom
     ),
     Olives(
         toppingName = R.string.topping_olives,
-        toppingDrawable = R.drawable.olives
+        toppingDrawable = R.drawable.olives,
+        pizzaOverlayImage = R.drawable.topping_olive
     ),
     Peppers(
         toppingName = R.string.topping_peppers,
-        toppingDrawable = R.drawable.peppers
+        toppingDrawable = R.drawable.peppers,
+        pizzaOverlayImage = R.drawable.topping_peppers
     ),
     Pepperoni(
         toppingName = R.string.topping_pepperoni,
-        toppingDrawable = R.drawable.pepperoni
+        toppingDrawable = R.drawable.pepperoni,
+        pizzaOverlayImage = R.drawable.topping_pepperoni
     ),
     Pineapple(
         toppingName = R.string.topping_pineapple,
-        toppingDrawable = R.drawable.pineapple
+        toppingDrawable = R.drawable.pineapple,
+        pizzaOverlayImage = R.drawable.topping_pineapple
     ),
 }
