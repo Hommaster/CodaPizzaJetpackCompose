7aa0035af110cd609c64eab4287e260b1fc5127d
Hommaster <ilya.pan.2017@mail.ru>
2024-04-26 12:20:26 +0300

    update function "price" and added the dependence of the cost on the type of pizza

--- app/src/main/java/com/example/codapizza/model/Pizza.kt
+++ app/src/main/java/com/example/codapizza/model/Pizza.kt
@@ -3,6 +3,7 @@ package com.example.codapizza.model
 import android.os.Parcelable
 import android.util.Log
 import com.example.codapizza.model.ToppingPlacement.*
+import kotlinx.parcelize.IgnoredOnParcel
 import kotlinx.parcelize.Parcelize
 import java.math.BigDecimal
 import kotlin.math.roundToInt
@@ -10,23 +11,37 @@ import kotlin.math.roundToInt
 @Parcelize
 data class Pizza(
     val toppings: Map<Topping, ToppingPlacement> = emptyMap(),
-    val sizePizza: SizePizza? = null
+    val sizePizza: SizePizza? = null,
+    val pizzaName: String?
 ): Parcelable {
 
+    @IgnoredOnParcel
+    private val pizzaNamePrice: BigDecimal = when(pizzaName) {
+        "Carbonara" -> 11.99
+        "Margherita" -> 9.99
+        else -> 9.99
+    }.toBigDecimal()
+
+    @IgnoredOnParcel
+    private val sizePizzaPrice: BigDecimal = when (sizePizza) {
+        SizePizza.Small -> 5.0
+        SizePizza.Average -> 6.0
+        SizePizza.Big -> 7.0
+        SizePizza.VeryBig -> 8.0
+        else -> 7.0
+    }.toBigDecimal()
+
+    @IgnoredOnParcel
+    private val toppingsPrice: BigDecimal = toppings.asSequence()
+        .sumOf { (_, toppingPlacement) ->
+            when (toppingPlacement) {
+                Left, Right -> 0.5
+                All -> 1.0
+            }
+        }.toBigDecimal()
+
     val price: BigDecimal
-        get() = 9.99.toBigDecimal() + (when (sizePizza) {
-            SizePizza.Small -> 5.0
-            SizePizza.Average -> 6.0
-            SizePizza.Big -> 7.0
-            SizePizza.VeryBig -> 8.0
-            else -> 7.0
-        }).toBigDecimal() + (toppings.asSequence()
-                .sumOf { (_, toppingPlacement) ->
-                when (toppingPlacement) {
-                    Left, Right -> 0.5
-                    All -> 1.0
-                }
-            }).toBigDecimal()
+        get() = pizzaNamePrice + sizePizzaPrice + toppingsPrice
 
     fun withTopping(topping: Topping, placement: ToppingPlacement?): Pizza {
         return copy(
