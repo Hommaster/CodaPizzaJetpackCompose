9f0a22ef2cc492297a6f9bfdde7133446ec2338d
Hommaster <ilya.pan.2017@mail.ru>
2024-04-26 12:17:31 +0300

    upgrade UI project

---
+++ app/src/main/java/com/example/codapizza/theme/AppTheme.kt
@@ -0,0 +1,20 @@
+package com.example.codapizza.theme
+
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.lightColors
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+
+@Composable
+fun AppTheme(
+    content: @Composable () -> Unit
+) = MaterialTheme(
+    colors = lightColors(
+        primary = Color.LightGray,
+        primaryVariant = Color(0xFFA6262E),
+        secondary = Color(0xFF03C4DD),
+        secondaryVariant = Color(0xFF03B2C9),
+        )
+) {
+    content()
+}
