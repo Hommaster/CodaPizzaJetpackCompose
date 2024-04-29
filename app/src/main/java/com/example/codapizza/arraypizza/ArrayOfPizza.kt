a83c6e8991b9f515d20faf6c3cf526936b3c1f6c
Hommaster <ilya.pan.2017@mail.ru>
2024-04-29 10:47:38 +0300

    update Ui for main page v. 1.1

--- app/src/main/java/com/example/codapizza/arraypizza/ArrayOfPizza.kt
+++ app/src/main/java/com/example/codapizza/arraypizza/ArrayOfPizza.kt
@@ -1,6 +1,8 @@
 package com.example.codapizza.arraypizza
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -9,14 +11,18 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import com.example.codapizza.model.Pizzas
@@ -50,21 +56,36 @@ fun BoxOfPizza(
     pizza: Pizzas,
     onClick: () -> Unit
 ) {
-    Row(
+    Column(
         modifier = Modifier
-            .fillMaxSize()
+            .background(Color.LightGray)
+            .clip(RoundedCornerShape(13.dp))
+            .border(2.dp, Color.Green, RoundedCornerShape(13.dp))
             .clickable { onClick() }
     ){
-            Image(
-                modifier = Modifier
-                    .align(Alignment.CenterVertically)
-                    .size(50.dp),
-                painter = painterResource(id = pizza.pizzaImage),
-                contentDescription = stringResource(id = pizza.pizzaName)
-            )
+        Image(
+            modifier = Modifier
+                .align(Alignment.CenterHorizontally)
+                .size(250.dp),
+            painter = painterResource(id = pizza.pizzaImage),
+            contentDescription = stringResource(id = pizza.pizzaName)
+        )
+        Row(
+            modifier = Modifier
+                .fillMaxSize()
+                .clickable { onClick() }
+        ){
             Column {
-                Text(text = stringResource(id = pizza.pizzaName))
-                Text(text = stringResource(id = pizza.pizzaIngredients))
+                Text(
+                    text = stringResource(id = pizza.pizzaName),
+                    fontStyle = FontStyle.Italic,
+                    style = MaterialTheme.typography.body1
+                )
+                Text(
+                    text = stringResource(id = pizza.pizzaIngredients),
+                    style = MaterialTheme.typography.body2
+                )
             }
         }
     }
+}
