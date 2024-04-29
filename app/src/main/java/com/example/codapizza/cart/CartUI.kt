a610de4fed3b3ce53320219abd4fdb98343fb15d
Hommaster <ilya.pan.2017@mail.ru>
2024-04-29 21:51:48 +0300

    created new file for composable components about cart

---
+++ app/src/main/java/com/example/codapizza/cart/CartUI.kt
@@ -0,0 +1,34 @@
+package com.example.codapizza.cart
+
+import android.content.Intent
+import android.net.Uri
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+import androidx.core.content.ContextCompat.startActivity
+import androidx.navigation.NavHostController
+
+@Composable
+fun CartUI(navController: NavHostController) {
+    Column(
+        modifier = Modifier
+            .padding(20.dp, 100.dp)
+    ) {
+        Text(
+            text = "Корзина пуста"
+        )
+        Text(
+            text = "И находится в технической доработке"
+        )
+        TextButton(onClick = { 
+            navController.navigate("screen_1")
+        }) {
+            Text(text = "Вернуться в меню")
+        }
+    }
+}
