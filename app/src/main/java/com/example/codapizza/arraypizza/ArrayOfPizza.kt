bcdc9c8092af2f6bc7a1178280053f5b027259d2
Hommaster <ilya.pan.2017@mail.ru>
2024-05-01 22:56:37 +0300

    added images for items NavigationDrawerItems, navigation from this items and update UI main page

--- app/src/main/java/com/example/codapizza/arraypizza/ArrayOfPizza.kt
+++ app/src/main/java/com/example/codapizza/arraypizza/ArrayOfPizza.kt
@@ -3,9 +3,14 @@ package com.example.codapizza.arraypizza
 import android.content.Intent
 import android.net.Uri
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 //noinspection UsingMaterialAndMaterial3Libraries
@@ -18,12 +23,15 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.ModalDrawerSheet
 import androidx.compose.material3.ModalNavigationDrawer
 import androidx.compose.material3.NavigationDrawerItem
+import androidx.compose.material3.Text
 import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -51,35 +59,83 @@ fun ArrayOfPizza(navController: NavHostController) {
     val drawerState = rememberDrawerState(DrawerValue.Closed)
     val scope = rememberCoroutineScope()
     ModalNavigationDrawer(
-        modifier = Modifier
-            .padding(0.dp, 25.dp),
         drawerState = drawerState,
         drawerContent = {
-            ModalDrawerSheet{
+            ModalDrawerSheet(
+                modifier = Modifier
+                    .padding(0.dp, 40.dp, 30.dp, 0.dp)
+            ){
                 NavigationDrawerItems.entries.forEach { item ->
-                    Row{
-                        NavigationDrawerItem(
-                            label= { androidx.compose.material3.Text(stringResource(id = item.nameItem), fontSize = 22.sp) },
-                            selected = selectedItem.value== stringResource(id = item.nameItem),
-                            onClick = {
-                                scope.launch { drawerState.close() }
-                                item.navigate
+                    NavigationDrawerItem(
+                        label= {
+                            Row {
+                                Column(
+                                    modifier = Modifier
+                                        .size(250.dp, 30.dp)
+                                ){
+                                    Text(
+                                        stringResource(id = item.nameItem),
+                                        fontSize = 22.sp
+                                    )
+                                }
+                                Image(
+                                    modifier = Modifier
+                                        .padding(10.dp, 0.dp)
+                                        .size(35.dp),
+                                    painter = painterResource(id = item.imageItem),
+                                    contentDescription = stringResource(id = item.nameItem)
+                                )
                             }
-                        )
-                        Image(
-                            painter = painterResource(id = item.imageItem), 
-                            contentDescription = stringResource(id = item.nameItem)
-                        )
-                    }
+                        },
+                        selected = false,
+//                        selected = selectedItem.value== stringResource(id = item.nameItem),
+                        onClick = {
+                            scope.launch { drawerState.close() }
+                            when(item.nameItem) {
+                                R.string.cart -> {
+                                    navController.navigate("cart_screen")
+                                }
+
+                                R.string.contact_telegram -> {
+                                    val browserIntent = Intent(
+                                        Intent.ACTION_VIEW,
+                                        Uri.parse(urlTG)
+                                    )
+                                    startActivity(context, browserIntent, null)
+                                }
+
+                                R.string.contact_vk -> {
+                                    val browserIntent = Intent(
+                                        Intent.ACTION_VIEW,
+                                        Uri.parse(urlVK)
+                                    )
+                                    startActivity(context, browserIntent, null)
+                                }
+                            }
+                        }
+                    )
                 }
             }
         },
         content={
-            Column{
-                Row{
-                    TopAppBar {
-                        IconButton(onClick = {scope.launch {drawerState.open()}},
-                            content = { Icon(Icons.Filled.Menu, "Меню") }
+            Column(
+                modifier = Modifier
+                    .background(Color.Black)
+            ){
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(80.dp)
+                ){
+                    TopAppBar(
+                        modifier = Modifier
+                            .fillMaxHeight()
+                    ) {
+                        IconButton(
+                            modifier = Modifier
+                                .align(Alignment.Bottom),
+                            onClick = {scope.launch {drawerState.open()}},
+                            content = { Icon(Icons.Filled.Menu, "Menu") }
                         )
                     }
                 }
