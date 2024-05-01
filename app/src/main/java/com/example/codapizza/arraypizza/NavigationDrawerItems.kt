0c6351362e1835ab938a08431cd21b16e8d84630
Hommaster <ilya.pan.2017@mail.ru>
2024-05-01 22:55:14 +0300

    added Cart and VK for NavigationDrawerItems

--- app/src/main/java/com/example/codapizza/arraypizza/NavigationDrawerItems.kt
+++ app/src/main/java/com/example/codapizza/arraypizza/NavigationDrawerItems.kt
@@ -7,22 +7,20 @@ import androidx.navigation.NavHostController
 import com.example.codapizza.R
 
 enum class NavigationDrawerItems(
-    val navigate: @Composable () -> Unit,
     @StringRes val nameItem: Int,
     @DrawableRes val imageItem: Int,
 ) {
+    Cart(
+        nameItem = R.string.cart,
+        imageItem = R.drawable.cart
+    ),
     Telegram(
         nameItem = R.string.contact_telegram,
         imageItem = R.drawable.telegram,
-        navigate = {
-            val context = androidx.compose.ui.platform.LocalContext.current
-            val urlTG = "https://t.me/Pankratar"
-            val browserIntent = android.content.Intent(
-                android.content.Intent.ACTION_VIEW,
-                android.net.Uri.parse(urlTG)
-            )
-            androidx.core.content.ContextCompat.startActivity(context, browserIntent, null)
-        }
-    )
+    ),
+    VK(
+        nameItem = R.string.contact_vk,
+        imageItem = R.drawable.vk
+    ),
 
 }
