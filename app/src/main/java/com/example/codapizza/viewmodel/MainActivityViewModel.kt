c9f63cc34bdf508712fba9b678fffc5d6400efb9
Hommaster <ilya.pan.2017@mail.ru>
2024-05-07 23:30:09 +0300

    created ViewModel for save state of database Room about Orders

---
+++ app/src/main/java/com/example/codapizza/viewmodel/MainActivityViewModel.kt
@@ -0,0 +1,39 @@
+package com.example.codapizza.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.codapizza.cart.database.Order
+import com.example.codapizza.cart.repository.OrderRepository
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+
+class MainActivityViewModel : ViewModel() {
+
+    private val orderRepository = OrderRepository.get()
+
+    private val _orders: MutableStateFlow<List<Order>> = MutableStateFlow(emptyList())
+
+    val orders: StateFlow<List<Order>>
+        get() = _orders.asStateFlow()
+
+    init {
+        viewModelScope.launch {
+            orderRepository.getOrders().collect{
+                _orders.value = it
+            }
+        }
+    }
+
+    fun getOrders() {
+        orderRepository.getOrders()
+    }
+
+    suspend fun addOrder(order: Order) {
+        orderRepository.addOrder(order)
+    }
+
+
+
+}
