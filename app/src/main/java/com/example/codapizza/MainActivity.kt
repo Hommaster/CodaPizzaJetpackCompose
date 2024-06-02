97b6e7f1917548176814970bd50d2b3ef50d0f1e
Hommaster <ilya.pan.2017@mail.ru>
2024-06-02 17:49:22 +0300

    added 2 new arguments for PizzaBuilderScreen.kt

--- app/src/main/java/com/example/codapizza/MainActivity.kt
+++ app/src/main/java/com/example/codapizza/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.codapizza
 
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.runtime.MutableState
 import androidx.core.view.WindowCompat
@@ -19,8 +21,11 @@ import com.example.codapizza.cart.EmptyCartUI
 import com.example.codapizza.theme.AppTheme
 import com.example.codapizza.pizza.PizzaBuilderScreen
 import com.example.codapizza.cart.viewmodel.MainActivityViewModel
+import com.example.codapizza.model.Pizza
+import com.example.codapizza.model.PizzaType
 
 class MainActivity : AppCompatActivity() {
+    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -38,16 +43,24 @@ class MainActivity : AppCompatActivity() {
 
             NavHost(navController = navController, startDestination = "screen_1") {
                 composable(
-                    "screen_2/{pizza_name}",
+                    "screen_2/{pizza_name}/{pizza_from_order}/{order_id}",
                     arguments = listOf(
                         navArgument(name = "pizza_name"){
+                            type = NavType.StringType
+                        },
+                        navArgument(name = "pizza_from_order"){
+                            type = PizzaType()
+                        },
+                        navArgument(name = "order_id"){
                             type = NavType.StringType
                         }
                     )
                 ) { backstackEntry ->
                     PizzaBuilderScreen(
                         navController,
-                        pizzaName = backstackEntry.arguments?.getString("pizza_name")
+                        pizzaFromOrder = backstackEntry.arguments?.getParcelable("pizza_from_order", Pizza::class.java),
+                        pizzaName = backstackEntry.arguments?.getString("pizza_name"),
+                        orderID = backstackEntry.arguments?.getString("order_id")
                     )
                 }
                 composable("screen_1") {
