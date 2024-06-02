c8b916a0800a77e06953b9ecce63d40966c23651
Hommaster <ilya.pan.2017@mail.ru>
2024-06-02 17:53:47 +0300

    created new migration to RoomDatabase for added new column about quantity of order

--- app/src/main/java/com/example/codapizza/cart/database/OrderDatabase.kt
+++ app/src/main/java/com/example/codapizza/cart/database/OrderDatabase.kt
@@ -3,11 +3,19 @@ package com.example.codapizza.cart.database
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
+import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
 
-@Database(entities = [Orders::class], version = 2, exportSchema = false)
+@Database(entities = [Orders::class], version = 3, exportSchema = false)
 @TypeConverters(OrderTypeConverter::class)
 abstract class OrderDatabase : RoomDatabase() {
 
     abstract fun orderDao(): OrderDao
 
+}
+
+val migration_2_3: Migration = object : Migration(2, 3){
+    override fun migrate(db: SupportSQLiteDatabase) {
+        db.execSQL("ALTER TABLE orders ADD COLUMN quantity INT DEFAULT '1'")
+    }
 }
