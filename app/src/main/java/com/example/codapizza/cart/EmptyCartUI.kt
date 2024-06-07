079aee78962680797d6cdfa7813bed4f047c2f5f
Hommaster <ilya.pan.2017@mail.ru>
2024-06-07 18:21:41 +0300

    updated UI for EmptyCartUI.kt

--- app/src/main/java/com/example/codapizza/cart/EmptyCartUI.kt
+++ app/src/main/java/com/example/codapizza/cart/EmptyCartUI.kt
@@ -1,27 +1,99 @@
 package com.example.codapizza.cart
 
-import android.util.Log
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardColors
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextIndent
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
 import com.example.codapizza.R
+
+@Preview
+@Composable
+fun PreviewEmptyCartUI(
+    
+){
+    EmptyCartUI(navController = rememberNavController())
+}
 
 @Composable
 fun EmptyCartUI(
     navController: NavController
 ){
-    Log.d("INFOaboutScreen", "screen emptyinfo")
-    Column(){
-        Text(
-            text = stringResource(id = R.string.cart_is_empty)
-        )
-        TextButton(onClick = {
-            navController.navigate("screen_1")
-        }) {
-            Text(text = stringResource(id = R.string.back_to_menu))
+    Column(
+        modifier = Modifier
+            .background(colorResource(id = R.color.black))
+            .fillMaxSize(),
+    ) {
+        Card(
+            modifier = Modifier
+                .padding(0.dp, 0.dp),
+            shape = RoundedCornerShape(0.dp, 0.dp, 15.dp, 15.dp),
+        ) {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(colorResource(id = R.color.orange))
+                    .height(80.dp)
+            ) {
+                
+            }
+        }
+        
+        Column(
+            modifier = Modifier
+                .padding(30.dp, 250.dp)
+                .align(Alignment.CenterHorizontally)
+        ) {
+            Card(
+                shape = RoundedCornerShape(15.dp),
+                colors = CardColors(colorResource(id = R.color.springgreen), Color.Cyan, Color.Cyan, Color.Cyan)
+            ) {
+                Text(
+                    color = colorResource(id = R.color.orange),
+                    text = stringResource(id = R.string.cart_is_empty),
+                    fontStyle = FontStyle.Italic,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 18.sp,
+                    style = TextStyle(textIndent = TextIndent(15.sp, 30.sp))
+                    )
+                Row(
+                    modifier = Modifier
+                        .align(Alignment.CenterHorizontally)
+                ){
+                    TextButton(onClick = {
+                        navController.popBackStack("screen_1", false)
+                    }) {
+                        Text(
+                            text = stringResource(id = R.string.back_to_menu),
+                            fontStyle = FontStyle.Italic,
+                            fontWeight = FontWeight.Bold,
+                            fontSize = 20.sp,
+                        )
+                    }
+                }
+            }
         }
     }
 }
