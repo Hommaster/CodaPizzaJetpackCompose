1445b4d9de0eccd4490b10cf1e91287d9dbe73b3
Hommaster <ilya.pan.2017@mail.ru>
2024-06-09 00:51:17 +0300

    cleared code

--- app/src/main/java/com/example/codapizza/arraypizza/BoxOfPizza.kt
+++ app/src/main/java/com/example/codapizza/arraypizza/BoxOfPizza.kt
@@ -24,7 +24,6 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.codapizza.R
 import com.example.codapizza.model.Pizzas
