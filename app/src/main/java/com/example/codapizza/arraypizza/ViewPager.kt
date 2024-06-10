7d351fab385866dc69cceab002ccb668afe61f72
Hommaster <ilya.pan.2017@mail.ru>
2024-06-11 01:29:33 +0300

    creating a separate file for ViewPager and adding information about snacks

---
+++ app/src/main/java/com/example/codapizza/arraypizza/ViewPager.kt
@@ -0,0 +1,134 @@
+package com.example.codapizza.arraypizza
+
+import android.net.Uri
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.pager.PagerState
+import androidx.compose.foundation.pager.VerticalPager
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import com.example.codapizza.R
+import com.example.codapizza.model.Pizza
+import com.example.codapizza.model.Pizzas
+import com.example.codapizza.snack.SnackInfo
+import com.google.gson.Gson
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
+
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+fun ViewPager(
+    pagerState: PagerState,
+    scope: CoroutineScope,
+    navController: NavController,
+) {
+    Row(
+        modifier = Modifier
+            .wrapContentHeight()
+            .fillMaxWidth()
+            .padding(bottom = 8.dp),
+        horizontalArrangement = Arrangement.Center
+    ){
+        var color1: Color = Color.White
+        var color2: Color = Color.White
+        repeat(pagerState.pageCount) {
+            color1 = if(pagerState.currentPage == 0) colorResource(id = R.color.orange) else Color.White
+            color2 = if(pagerState.currentPage == 1) colorResource(id = R.color.orange) else Color.White
+        }
+        Button(
+            modifier = Modifier
+                .padding(2.dp),
+            shape = RoundedCornerShape(15.dp),
+            onClick = {
+                scope.launch {
+                    pagerState.animateScrollToPage(0)
+                }
+            },
+            colors = ButtonDefaults.buttonColors(color1)
+        ) {
+            Text(
+                modifier = Modifier
+                    .background(color1),
+                text = stringResource(id = R.string.view_pager_info_pizzas)
+            )
+        }
+        Button(
+            modifier = Modifier
+                .padding(2.dp),
+            shape = RoundedCornerShape(15.dp),
+            onClick = {
+                scope.launch {
+                    pagerState.animateScrollToPage(1)
+                }
+            },
+            colors = ButtonDefaults.buttonColors(color2)
+        ) {
+            Text(
+                modifier = Modifier
+                    .background(color2),
+                text = stringResource(id = R.string.view_pager_info_snacks)
+            )
+        }
+    }
+
+    VerticalPager(
+        state = pagerState
+    ) {page ->
+        when(page){
+            0 -> {
+                LazyColumn {
+                    items(Pizzas.entries.toTypedArray()) { pizza ->
+                        val pizzaName: String = stringResource(id = pizza.pizzaName)
+                        val pizzaWithArrayOfPizza = Pizza(
+                            pizzaName = "pizzaWithArrayOfPizza"
+                        )
+                        val json = Uri.encode(Gson().toJson(pizzaWithArrayOfPizza))
+                        BoxOfProduct(
+                            pizza = pizza,
+                            snack = null,
+                            onClick = {
+                                navController.navigate("screen_2/$pizzaName/$json/1") {
+                                    popUpTo("screen_1")
+                                }
+                            }
+                        )
+                    }
+                }
+            }
+            1 -> {
+                LazyColumn {
+                    items(SnackInfo.entries.toTypedArray()) { snack ->
+//                                    val pizzaName: String = stringResource(id = pizza.pizzaName)
+//                                    val pizzaWithArrayOfPizza = Pizza(
+//                                        pizzaName = "pizzaWithArrayOfPizza"
+//                                    )
+//                                    val json = Uri.encode(Gson().toJson(pizzaWithArrayOfPizza))
+                        BoxOfProduct(
+                            pizza = null,
+                            snack = snack,
+                            onClick = {
+
+                            }
+                        )
+                    }
+                }
+            }
+        }
+    }
+}
