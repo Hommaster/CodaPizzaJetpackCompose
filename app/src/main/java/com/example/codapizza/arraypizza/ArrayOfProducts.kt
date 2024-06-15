4feed3fc298e260a69b4681643d926b126297621
Hommaster <ilya.pan.2017@mail.ru>
2024-06-16 00:44:46 +0300

    created buttons on the main screen that appear when there is an order in the cart, for quickly moving to the cart

--- app/src/main/java/com/example/codapizza/arraypizza/ArrayOfProducts.kt
+++ app/src/main/java/com/example/codapizza/arraypizza/ArrayOfProducts.kt
@@ -2,9 +2,11 @@ package com.example.codapizza.arraypizza
 
 import android.content.Intent
 import android.net.Uri
+import android.util.Log
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
@@ -18,6 +20,9 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.TopAppBar
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.Icon
@@ -29,9 +34,14 @@ import androidx.compose.material3.NavigationDrawerItemDefaults
 import androidx.compose.material3.Text
 import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
@@ -58,126 +68,167 @@ fun ArrayOfProducts(
     val drawerState = rememberDrawerState(DrawerValue.Closed)
     val scope = rememberCoroutineScope()
 
-    ModalNavigationDrawer(
-        drawerState = drawerState,
-        drawerContent = {
-            ModalDrawerSheet(
-                modifier = Modifier
-                    .padding(0.dp, 40.dp, 67.dp, 0.dp),
-                drawerContainerColor = colorResource(id = R.color.orange)
-            ){
-                NavigationDrawerItems.entries.forEach { item ->
-                    NavigationDrawerItem(
-                        colors = NavigationDrawerItemDefaults.colors(
-                            unselectedContainerColor = colorResource(R.color.orange)
-                        ),
-                        label= {
-                            Row{
-                                Image(
-                                    modifier = Modifier
-                                        .size(35.dp),
-                                    painter = painterResource(id = item.imageItem),
-                                    contentDescription = stringResource(id = item.nameItem)
-                                )
-                                Column(
-                                    modifier = Modifier
-                                        .padding(20.dp, 0.dp)
-                                        .size(250.dp, 30.dp)
-                                ){
-                                    Text(
-                                        stringResource(id = item.nameItem),
-                                        fontSize = 22.sp
+    val haveOrder: MutableState<Int> = rememberSaveable {
+        mutableStateOf(0)
+    }
+
+        ModalNavigationDrawer(
+            drawerState = drawerState,
+            drawerContent = {
+                ModalDrawerSheet(
+                    modifier = Modifier
+                        .padding(0.dp, 40.dp, 67.dp, 0.dp),
+                    drawerContainerColor = colorResource(id = R.color.orange)
+                ) {
+                    NavigationDrawerItems.entries.forEach { item ->
+                        NavigationDrawerItem(
+                            colors = NavigationDrawerItemDefaults.colors(
+                                unselectedContainerColor = colorResource(R.color.orange)
+                            ),
+                            label = {
+                                Row {
+                                    Image(
+                                        modifier = Modifier
+                                            .size(35.dp),
+                                        painter = painterResource(id = item.imageItem),
+                                        contentDescription = stringResource(id = item.nameItem)
                                     )
+                                    Column(
+                                        modifier = Modifier
+                                            .padding(20.dp, 0.dp)
+                                            .size(250.dp, 30.dp)
+                                    ) {
+                                        Text(
+                                            stringResource(id = item.nameItem),
+                                            fontSize = 22.sp
+                                        )
+                                    }
                                 }
-                            }
-                        },
-                        selected = false,
-                        onClick = {
-                            scope.launch {
-                                drawerState.close()
-                            }
-                            when(item.nameItem) {
-                                R.string.cart -> {
-                                    scope.launch {
-                                        mainActivityViewModel.orders.collect{
-                                            if(it.isEmpty()) {
-                                                navController.navigate("cart_screen_empty") {
-                                                    popUpTo("screen_1")
-                                                }
-                                            }
-                                            else {
-                                                navController.navigate("cart_screen") {
-                                                    popUpTo("screen_1")
+                            },
+                            selected = false,
+                            onClick = {
+                                scope.launch {
+                                    drawerState.close()
+                                }
+                                when (item.nameItem) {
+                                    R.string.cart -> {
+                                        scope.launch {
+                                            mainActivityViewModel.orders.collect {
+                                                if (it.isEmpty()) {
+                                                    navController.navigate("cart_screen_empty") {
+                                                        popUpTo("screen_1")
+                                                    }
+                                                } else {
+                                                    navController.navigate("cart_screen") {
+                                                        popUpTo("screen_1")
+                                                    }
                                                 }
                                             }
                                         }
                                     }
-                                }
-                                R.string.order_history -> {
-                                    navController.navigate("order_history") {
-                                        popUpTo("screen_1")
+
+                                    R.string.order_history -> {
+                                        navController.navigate("order_history") {
+                                            popUpTo("screen_1")
+                                        }
+                                    }
+
+                                    R.string.contact_telegram -> {
+                                        val browserIntent = Intent(
+                                            Intent.ACTION_VIEW,
+                                            Uri.parse(urlTG)
+                                        )
+                                        startActivity(context, browserIntent, null)
+                                    }
+
+                                    R.string.contact_vk -> {
+                                        val browserIntent = Intent(
+                                            Intent.ACTION_VIEW,
+                                            Uri.parse(urlVK)
+                                        )
+                                        startActivity(context, browserIntent, null)
                                     }
                                 }
-
-                                R.string.contact_telegram -> {
-                                    val browserIntent = Intent(
-                                        Intent.ACTION_VIEW,
-                                        Uri.parse(urlTG)
+                            }
+                        )
+                    }
+                }
+            },
+            content = {
+                Box {
+                    LaunchedEffect(key1 = Unit) {
+                        scope.launch {
+                            mainActivityViewModel.orders.collect{
+                                it.size
+                                haveOrder.value = it.size
+                            }
+                        }
+                    }
+                    Column(
+                        modifier = Modifier
+                            .background(colorResource(id = R.color.black))
+                    ) {
+                        Card(
+                            modifier = Modifier
+                                .padding(0.dp, 0.dp),
+                            shape = RoundedCornerShape(0.dp, 0.dp, 15.dp, 15.dp),
+                        ) {
+                            Row(
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .height(80.dp),
+                            ) {
+                                TopAppBar(
+                                    modifier = Modifier
+                                        .fillMaxHeight()
+                                ) {
+                                    IconButton(
+                                        modifier = Modifier
+                                            .align(Alignment.Bottom),
+                                        onClick = { scope.launch { drawerState.open() } },
+                                        content = { Icon(Icons.Filled.Menu, "Menu") }
                                     )
-                                    startActivity(context, browserIntent, null)
-                                }
-
-                                R.string.contact_vk -> {
-                                    val browserIntent = Intent(
-                                        Intent.ACTION_VIEW,
-                                        Uri.parse(urlVK)
-                                    )
-                                    startActivity(context, browserIntent, null)
                                 }
                             }
                         }
-                    )
-                }
-            }
-        },
-        content={
-            Column(
-                modifier = Modifier
-                    .background(colorResource(id = R.color.black))
-            ){
-                Card(
-                    modifier = Modifier
-                        .padding(0.dp, 0.dp),
-                    shape = RoundedCornerShape(0.dp, 0.dp, 15.dp, 15.dp),
-                ) {
-                    Row(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .height(80.dp),
-                    ){
-                        TopAppBar(
+
+                        val pagerState = rememberPagerState(pageCount = {
+                            2
+                        })
+                        ViewPager(
+                            pagerState = pagerState,
+                            scope = scope,
+                            navController = navController
+                        )
+                    }
+                    if(haveOrder.value > 0) {
+                        Box(
                             modifier = Modifier
-                                .fillMaxHeight()
+                                .background(Color.Transparent)
+                                .padding(270.dp, 720.dp, 0.dp, 0.dp)
+                                .size(120.dp, 60.dp),
                         ) {
-                            IconButton(
+                            Button(
                                 modifier = Modifier
-                                    .align(Alignment.Bottom),
-                                onClick = {scope.launch {drawerState.open()}},
-                                content = { Icon(Icons.Filled.Menu, "Menu") }
-                            )
+                                    .padding(2.dp),
+                                shape = RoundedCornerShape(15.dp),
+                                onClick = {
+                                    navController.navigate("cart_screen") {
+                                        popUpTo("screen_1")
+                                    }
+                                },
+                                colors = ButtonDefaults.buttonColors(colorResource(id = R.color.black))
+                            ) {
+                                Text(
+                                    modifier = Modifier
+                                        .background(colorResource(id = R.color.black)),
+                                    text = stringResource(id = R.string.cart),
+                                    color = colorResource(id = R.color.orange)
+                                )
+                            }
                         }
                     }
                 }
-
-                val pagerState = rememberPagerState( pageCount = {
-                    2
-                })
-                ViewPager(
-                    pagerState = pagerState,
-                    scope = scope,
-                    navController = navController
-                )
             }
-        }
-    )
-}
+        )
+    }
