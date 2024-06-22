6e8f00146ed217a5f72954b49f8a9212f48dbc73
Hommaster <ilya.pan.2017@mail.ru>
2024-06-22 22:45:57 +0300

    creating PizzaBuilderScreen.kt not through pizzaName and direct string iteration, but through NavType PizzaInfoType

--- app/src/main/java/com/example/codapizza/productInfo/pizza/PizzaBuilderScreen.kt
+++ app/src/main/java/com/example/codapizza/productInfo/pizza/PizzaBuilderScreen.kt
@@ -11,23 +11,11 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.RoundedCornerShape
-//noinspection UsingMaterialAndMaterial3Libraries
-import androidx.compose.material.Button
-//noinspection UsingMaterialAndMaterial3Libraries
-import androidx.compose.material.ButtonDefaults
-//noinspection UsingMaterialAndMaterial3Libraries
-import androidx.compose.material.LocalTextStyle
-//noinspection UsingMaterialAndMaterial3Libraries
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
@@ -35,37 +23,25 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.TransformOrigin
 import androidx.compose.ui.graphics.graphicsLayer
-import androidx.compose.ui.res.colorResource
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.intl.Locale
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.text.style.TextMotion
-import androidx.compose.ui.text.toUpperCase
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.example.codapizza.R
-import com.example.codapizza.cart.database.Orders
 import com.example.codapizza.cart.swipetodismiss.SwipeToDismiss
+import com.example.codapizza.cart.viewmodel.MainActivityViewModel
+import com.example.codapizza.desygnfiles.BoxWithImageScrollToDismiss
 import com.example.codapizza.model.Pizza
 import com.example.codapizza.model.Pizzas
-import com.example.codapizza.model.Topping
-import com.example.codapizza.cart.viewmodel.MainActivityViewModel
-import com.example.codapizza.cart.viewmodel.OrderDetailViewModel
-import com.example.codapizza.desygnfiles.BoxWithImageScrollToDismiss
 import com.example.codapizza.model.Sauce
+import com.example.codapizza.model.Topping
 import com.example.codapizza.sauce.SauceCell
-import kotlinx.coroutines.launch
-import java.util.Date
-import java.util.UUID
-
 
 @Composable
 fun PizzaBuilderScreen(
     navController: NavController,
-    modifier: Modifier = Modifier,
+    pizzaInfo: Pizzas?,
     pizzaFromOrder: Pizza?,
-    orderID: String?,
-    pizzaName: String?
+    pizzaName: String?,
+    orderID: String?
 ) {
 
     var pizza by rememberSaveable {
@@ -79,21 +55,18 @@ fun PizzaBuilderScreen(
         }
     }
 
-    val nameOfPizza = if(pizzaFromOrder!!.pizzaName == "pizzaWithArrayOfPizza") pizzaName else pizzaFromOrder.pizzaName
-
     SwipeToDismiss(
         navController
     ) {
         Box {
             Column(
-                modifier = modifier
+                modifier = Modifier
                     .background(Color.Black)
                     .padding(0.dp, 30.dp)
             ) {
-                PizzasImage(
+                PizzaImages(
                     pizza = pizza,
-                    pizzaName = nameOfPizza,
-                    modifier = modifier
+                    pizzaInfo = pizzaInfo!!
                 )
                 SizeDialog(
                     pizza = pizza,
@@ -102,7 +75,7 @@ fun PizzaBuilderScreen(
                 ToppingList(
                     pizza = pizza,
                     onEditTopping = { pizza = it },
-                    modifier = modifier
+                    modifier = Modifier
                         .fillMaxWidth()
                         .weight(1f, fill = true)
                 )
@@ -111,8 +84,8 @@ fun PizzaBuilderScreen(
                     orderID = orderID,
                     mainActivityViewModel = MainActivityViewModel(),
                     navController = navController,
-                    pizzaName = nameOfPizza,
-                    modifier = modifier
+                    pizzaInfo = pizzaInfo,
+                    modifier = Modifier
                         .align(Alignment.CenterHorizontally)
                         .padding(10.dp)
                 )
@@ -123,7 +96,6 @@ fun PizzaBuilderScreen(
                 colorFilter = R.color.orange
             )
         }
-
     }
 }
 
@@ -155,7 +127,7 @@ private fun ToppingList(
             topping = it,
             placementIsChoose = pizza.isPlacement(it),
             onSetToppingPlacement = {placement ->
-                                    onEditTopping(pizza.withTopping(it, placement))
+                onEditTopping(pizza.withTopping(it, placement))
             },
             onDismissRequest = {
                 toppingBeingAdd = null
@@ -190,7 +162,7 @@ private fun ToppingList(
             )
         }
 
-        items(Sauce.entries.toTypedArray()) {sauce ->
+        items(Sauce.entries.toTypedArray()) { sauce ->
             SauceCell(
                 sauce = sauce,
                 quantity = pizza.sauces[sauce],
@@ -205,137 +177,6 @@ private fun ToppingList(
                         transformOrigin = TransformOrigin.Center
                     }
             )
-        }
-    }
-}
-
-@Composable
-private fun OrderButton(
-    mainActivityViewModel: MainActivityViewModel,
-    orderID: String?,
-    navController: NavController,
-    pizza: Pizza,
-    pizzaName: String?,
-    modifier: Modifier = Modifier
-) {
-
-    val orderDetailViewModel: OrderDetailViewModel? = if(orderID!="1") OrderDetailViewModel(UUID.fromString(orderID)) else null
-
-    val pizzas = when(pizzaName) {
-        stringResource(R.string.margherita) -> Pizzas.Margherita
-        stringResource(R.string.carbonara) -> Pizzas.Carbonara
-        stringResource(R.string.chicago) -> Pizzas.Chicago
-        else -> Pizzas.Margherita
-    }
-    val order: MutableState<Orders?> = rememberSaveable {
-        mutableStateOf(null)
-    }
-
-    val descriptionPizza = stringResource(id = pizzas.pizzaIngredients)
-
-    val coroutineScope = rememberCoroutineScope()
-
-    val infiniteTransition = rememberInfiniteTransition(label = "infinite transition")
-    val scale by infiniteTransition.animateFloat(
-        initialValue = 1f,
-        targetValue = 1.05f,
-        animationSpec = infiniteRepeatable(tween(15000), RepeatMode.Reverse),
-        label = "scale"
-    )
-
-    when(orderID){
-        "1" -> {
-            Button(
-                modifier = modifier
-                    .width(280.dp),
-                onClick = {
-                    coroutineScope.launch {
-                        val newOrder = Orders(
-                            id = UUID.randomUUID(),
-                            title = pizza.pizzaName!!,
-                            description = descriptionPizza,
-                            date = Date(),
-                            image = pizzas.pizzaImage,
-                            toppings = pizza.toppings,
-                            sauce = pizza.sauces,
-                            price = pizza.price.toFloat()
-                        )
-                        mainActivityViewModel.addOrder(newOrder)
-                        navController.popBackStack("screen_1", false)
-                    }
-                },
-                shape = RoundedCornerShape(23.dp),
-                colors = ButtonDefaults.buttonColors(contentColor = Color.White, backgroundColor = colorResource(
-                    id = R.color.orange
-                ))
-            ) {
-
-                val price = pizza.price
-                Log.d("changeSize", "$price")
-
-                Text(
-                    modifier = modifier
-                        .graphicsLayer {
-                            scaleX = scale
-                            scaleY = scale
-                            transformOrigin = TransformOrigin.Center
-                        },
-                    style = LocalTextStyle.current.copy(textMotion = TextMotion.Animated),
-                    text = stringResource(R.string.place_order_button, price)
-                        .toUpperCase(Locale.current),
-                    textAlign = TextAlign.Center
-                )
-            }
-        }
-        else -> {
-            Button(
-                modifier = modifier
-                    .width(280.dp),
-                onClick = {
-                    coroutineScope.launch {
-                        orderDetailViewModel!!.updateOrder { oldOrder->
-                            Log.d("info_order", pizza.toppings.toString())
-                            oldOrder.copy(
-                                date = Date(),
-                                toppings = pizza.toppings,
-                                price = pizza.price.toFloat()
-                            )
-                        }
-                    }
-                    coroutineScope.launch {
-                        orderDetailViewModel!!.order.collect{
-                            order.value = it
-                        }
-                    }
-                    coroutineScope.launch {
-                        mainActivityViewModel.updateOrder(order.value!!)
-                    }
-                    navController.navigate("cart_screen") {
-                        popUpTo("screen_1")
-                    }
-                },
-                shape = RoundedCornerShape(23.dp),
-                colors = ButtonDefaults.buttonColors(contentColor = Color.White, backgroundColor = colorResource(
-                    id = R.color.orange
-                ))
-            ) {
-
-                val price = pizza.price
-                Log.d("changeSize", "$price")
-
-                Text(
-                    modifier = modifier
-                        .graphicsLayer {
-                            scaleX = scale
-                            scaleY = scale
-                            transformOrigin = TransformOrigin.Center
-                        },
-                    style = LocalTextStyle.current.copy(textMotion = TextMotion.Animated),
-                    text = stringResource(R.string.change_order_button, price)
-                        .toUpperCase(Locale.current),
-                    textAlign = TextAlign.Center
-                )
-            }
         }
     }
 }
