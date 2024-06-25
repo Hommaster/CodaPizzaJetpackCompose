784f0ca6710823028bea28c091bfb8ebb9ec264b
Hommaster <ilya.pan.2017@mail.ru>
2024-06-25 15:41:33 +0300

    functionality has been expanded (product verification and processing of information about pizzas has been added)

--- app/src/main/java/com/example/codapizza/productInfo/snack/ProductInfoData.kt
+++ app/src/main/java/com/example/codapizza/productInfo/snack/ProductInfoData.kt
@@ -3,10 +3,17 @@ package com.example.codapizza.productInfo.snack
 import android.os.Build
 import android.os.Bundle
 import android.os.Parcelable
+import android.util.Log
 import androidx.annotation.RequiresApi
 import androidx.navigation.NavType
+import com.example.codapizza.model.Pizzas
+import com.example.codapizza.model.Sauce
+import com.example.codapizza.model.SizePizza
 import com.example.codapizza.model.Topping
 import com.example.codapizza.model.ToppingPlacement
+import com.example.codapizza.model.ToppingPlacement.All
+import com.example.codapizza.model.ToppingPlacement.Left
+import com.example.codapizza.model.ToppingPlacement.Right
 import com.google.gson.Gson
 import kotlinx.parcelize.IgnoredOnParcel
 import kotlinx.parcelize.Parcelize
@@ -15,35 +22,66 @@ import java.math.BigDecimal
 @Parcelize
 data class ProductInfoData(
     val toppings: Map<Topping, ToppingPlacement> = emptyMap(),
+    val sauces: Map<Sauce, Int> = emptyMap(),
     val productSize: SizeProductNotPizza = SizeProductNotPizza.Standard,
     val productName: Int?,
+    val sizePizza: SizePizza? = null,
+    val pizzaName: String?
 ): Parcelable {
 
-//    @IgnoredOnParcel
-//    private val pizzaNamePrice: BigDecimal = when(snackName) {
-//        "Carbonara" -> 11.99
-//        "Margherita" -> 9.99
-//        "Chicago" -> 10.99
-//        else -> 9.99
-//    }.toBigDecimal()
+    init {
+        Log.d("infoPizzaName", "$pizzaName")
+    }
 
     @IgnoredOnParcel
-    private val sizeSnackPrice: BigDecimal = when (productSize) {
-        SizeProductNotPizza.Standard -> 4.0
-        SizeProductNotPizza.Big -> 5.0
-    }.toBigDecimal()
+    private val productPrice: BigDecimal? = if(pizzaName != null && pizzaName != "pizzaWithArrayOfPizza") {
+        Pizzas.valueOf(pizzaName).price.toBigDecimal()
+    } else if (pizzaName == null && productName != null && productName != 0) {
+        when (productSize) {
+            SizeProductNotPizza.Standard -> 4.0
+            SizeProductNotPizza.Big -> 5.0
+        }.toBigDecimal()
+    } else null
 
     @IgnoredOnParcel
     private val toppingsPrice: BigDecimal = toppings.asSequence()
         .sumOf { (_, toppingPlacement) ->
             when (toppingPlacement) {
-                ToppingPlacement.Left, ToppingPlacement.Right -> 0.0
-                ToppingPlacement.All -> 1.20
+                Left, Right -> 0.5
+                All -> 1.0
+                else -> 0.0
+            }
+        }.toBigDecimal()
+
+    @IgnoredOnParcel
+    private val sizePizzaPrice: BigDecimal = when (sizePizza) {
+        SizePizza.Small -> 0.99
+        SizePizza.Average -> 6.0
+        SizePizza.Big -> 7.0
+        SizePizza.VeryBig -> 8.0
+        else -> 0.0
+    }.toBigDecimal()
+
+    @IgnoredOnParcel
+    private val sizeProductPrice: BigDecimal = when(productSize) {
+        SizeProductNotPizza.Standard -> 0.0
+        SizeProductNotPizza.Big -> 1.0
+        else -> 0.0
+    }.toBigDecimal()
+
+    @IgnoredOnParcel
+    private val saucesPrice: BigDecimal = sauces.asSequence()
+        .sumOf {
+                (_, quantity) ->
+            when(quantity) {
+                0 -> 0.0
+                1 -> 1.20
+                else -> quantity * 1.20
             }
         }.toBigDecimal()
 
     val price: BigDecimal
-        get() = sizeSnackPrice + toppingsPrice
+        get() = productPrice!! + toppingsPrice + saucesPrice + sizePizzaPrice + sizeProductPrice
 
     fun withTopping(topping: Topping, placement: ToppingPlacement?): ProductInfoData {
         return copy(
@@ -55,9 +93,25 @@ data class ProductInfoData(
         )
     }
 
-    fun changeSizePizza(size: SizeProductNotPizza): ProductInfoData {
+    fun withQuantity(sauce: Sauce, quantity: Int?): ProductInfoData {
+        return copy(
+            sauces = if(quantity == null) {
+                sauces - sauce
+            } else {
+                sauces + (sauce to quantity)
+            }
+        )
+    }
+
+    fun changeSizeProduct(size: SizeProductNotPizza): ProductInfoData {
         return copy(
             productSize = size
+        )
+    }
+
+    fun changeSizePizza(size: SizePizza): ProductInfoData {
+        return copy(
+            sizePizza = size
         )
     }
 
