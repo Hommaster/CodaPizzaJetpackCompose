f20818e268c570749e4eb4a7ecbb3079480a14d4
Hommaster <ilya.pan.2017@mail.ru>
2024-06-25 15:39:48 +0300

    added value about prices of Pizzas

--- app/src/main/java/com/example/codapizza/model/Pizzas.kt
+++ app/src/main/java/com/example/codapizza/model/Pizzas.kt
@@ -4,33 +4,41 @@ import android.os.Build
 import android.os.Bundle
 import android.os.Parcelable
 import androidx.annotation.DrawableRes
+import androidx.annotation.FloatRange
+import androidx.annotation.IntRange
+import androidx.annotation.IntegerRes
 import androidx.annotation.RequiresApi
 import androidx.annotation.StringRes
 import androidx.navigation.NavType
 import com.example.codapizza.R
 import com.google.gson.Gson
 import kotlinx.parcelize.Parcelize
+import org.checkerframework.common.value.qual.DoubleVal
 
 @Parcelize
 enum class Pizzas(
+    @IntegerRes val price: Int,
     @StringRes val pizzaName: Int,
     @StringRes val pizzaIngredients: Int,
     @DrawableRes val pizzaImage: Int,
     @StringRes val pricePizza: Int
 ): Parcelable {
     Margherita(
+        price = 14,
         pizzaName = R.string.margherita,
         pizzaIngredients = R.string.margherita_ingredients,
         pizzaImage = R.drawable.margherita,
         pricePizza = R.string.price_margherita
     ),
     Carbonara(
+        price = 16,
         pizzaName = R.string.carbonara,
         pizzaIngredients = R.string.carbonara_ingredients,
         pizzaImage = R.drawable.carbonara,
         pricePizza = R.string.price_carbonara
     ),
     Chicago(
+        price = 15,
         pizzaName = R.string.chicago,
         pizzaIngredients = R.string.chicago_ingredients,
         pizzaImage = R.drawable.chicago,
