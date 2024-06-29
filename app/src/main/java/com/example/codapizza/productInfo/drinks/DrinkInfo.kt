609e231096276adfd88fa2f1bc79a346d66209bf
Hommaster <ilya.pan.2017@mail.ru>
2024-06-29 23:13:19 +0300

    added lines to store the cost of the product and whether it is a drink

--- app/src/main/java/com/example/codapizza/productInfo/drinks/DrinkInfo.kt
+++ app/src/main/java/com/example/codapizza/productInfo/drinks/DrinkInfo.kt
@@ -1,25 +1,34 @@
 package com.example.codapizza.productInfo.drinks
 
 import androidx.annotation.DrawableRes
+import androidx.annotation.IntegerRes
 import androidx.annotation.StringRes
 import com.example.codapizza.R
 
 enum class DrinkInfo(
+    @IntegerRes val price: Int,
     @StringRes val drinkName: Int,
-    @DrawableRes val drinkImage: Int
+    @DrawableRes val drinkImage: Int,
+    val drinkInfo: Boolean
 ) {
 
     CocaCola(
+        price = 2,
         drinkName = R.string.coca_cola,
-        drinkImage = R.drawable.cola_05
+        drinkImage = R.drawable.cola_05,
+        drinkInfo = true
     ),
     Sprite(
+        price = 2,
         drinkName = R.string.sprite,
-        drinkImage = R.drawable.sprite_05
+        drinkImage = R.drawable.sprite_05,
+        drinkInfo = true
     ),
     Fanta(
+        price = 2,
         drinkName = R.string.fanta,
-        drinkImage = R.drawable.fanta_05
+        drinkImage = R.drawable.fanta_05,
+        drinkInfo = true
     ),
 
 }
--- app/src/main/java/com/example/codapizza/productInfo/snack/SnackInfo.kt
+++ app/src/main/java/com/example/codapizza/productInfo/snack/SnackInfo.kt
@@ -1,19 +1,26 @@
 package com.example.codapizza.productInfo.snack
 
 import androidx.annotation.DrawableRes
+import androidx.annotation.IntegerRes
 import androidx.annotation.StringRes
 import com.example.codapizza.R
 
 enum class SnackInfo(
+    @IntegerRes val price: Int,
     @StringRes val snackName: Int,
-    @DrawableRes val snackImage: Int
+    @DrawableRes val snackImage: Int,
+    val drinkInfo: Boolean
 ) {
     FrenchFries(
+        price = 4,
         snackName = R.string.french_fries,
-        snackImage = R.drawable.french_fries_standart
+        snackImage = R.drawable.french_fries_standart,
+        drinkInfo = false
     ),
     Nuggets(
+        price = 4,
         snackName = R.string.Nuggets,
-        snackImage = R.drawable.nuggets_small
+        snackImage = R.drawable.nuggets_small,
+        drinkInfo = false
     )
 }
