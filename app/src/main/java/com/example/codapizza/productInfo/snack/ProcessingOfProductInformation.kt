246281571bd23bcee7d6b30f39e86ef1872ff25a
Hommaster <ilya.pan.2017@mail.ru>
2024-06-30 22:54:16 +0300

    more compact and less expensive code for obtaining product information to build a product collection screen and added new arguments productPrice, drinkInfoProduct, notPizzaName, productIDFromOrder, and new functions getPriceProduct, getDrinkInfo, getNotPizzaName, getProductIDFromOrder. Now the class takes one more argument, it is needed to determine which product class is on the screen and what needs to be collected for it

--- app/src/main/java/com/example/codapizza/productInfo/snack/ProcessingOfProductInformation.kt
+++ app/src/main/java/com/example/codapizza/productInfo/snack/ProcessingOfProductInformation.kt
@@ -1,42 +1,51 @@
 package com.example.codapizza.productInfo.snack
 
-import android.util.Log
 import com.example.codapizza.model.Pizzas
 import com.example.codapizza.productInfo.drinks.DrinkInfo
 
 class ProcessingOfProductInformation(
     productName: String,
-    pizzaName: String
+    pizzaName: String,
+    productID: Int
 ) {
     private var productImage: Int = 0
-    private var productDescription: Int = 0
+    private var productName: Int = 0
     private var productIngredients: Int = 0
+    private var productPrice: Int = 0
+    private var drinkInfoProduct: Boolean = false
+    private var notPizzaName: Int? = null
+    private var productIDFromOrder: Int = -1
 
 
-    private fun checkProductClass(productName: String, pizzaName: String) {
-        Log.d("info_order", "$productName , $pizzaName")
-        productImage = if(productName != "null") {
-            try {
-                DrinkInfo.valueOf(productName).drinkImage
-            } catch (e: Exception) {
-                SnackInfo.valueOf(productName).snackImage
+    private fun checkProductClass(productName: String, pizzaName: String, productID: Int) {
+        when (productID) {
+            0 -> {
+                productImage = Pizzas.valueOf(pizzaName).pizzaImage
+                this.productName = Pizzas.valueOf(pizzaName).pizzaName
+                productIngredients = Pizzas.valueOf(pizzaName).pizzaIngredients
+                productPrice = Pizzas.valueOf(pizzaName).price
+                drinkInfoProduct = false
             }
-        } else {
-            Pizzas.valueOf(pizzaName).pizzaImage
-        }
-        productDescription = if(productName != "null") {
-            try {
-                DrinkInfo.valueOf(productName).drinkName
-            } catch (e: Exception) {
-                SnackInfo.valueOf(productName).snackName
+
+            1 -> {
+                productImage = SnackInfo.valueOf(productName).snackImage
+                this.productName = SnackInfo.valueOf(productName).snackName
+                productIngredients = 0
+                productPrice = SnackInfo.valueOf(productName).price
+                drinkInfoProduct = SnackInfo.valueOf(productName).drinkInfo
+                notPizzaName = SnackInfo.valueOf(productName).snackName
+                productIDFromOrder = SnackInfo.valueOf(productName).snackName
             }
-        } else {
-            Pizzas.valueOf(pizzaName).pizzaName
-        }
-        productIngredients = if(pizzaName != "null") {
-            Pizzas.valueOf(pizzaName).pizzaIngredients
-        } else {
-            0
+
+            2 -> {
+                productImage = DrinkInfo.valueOf(productName).drinkImage
+                this.productName = DrinkInfo.valueOf(productName).drinkName
+                productIngredients = 0
+                productPrice = DrinkInfo.valueOf(productName).price
+                drinkInfoProduct = DrinkInfo.valueOf(productName).drinkInfo
+                notPizzaName = DrinkInfo.valueOf(productName).drinkName
+                productIDFromOrder = DrinkInfo.valueOf(productName).drinkName
+            }
         }
     }
 
@@ -44,15 +53,32 @@ class ProcessingOfProductInformation(
         return productImage
     }
 
-    fun getProductDescription(): Int {
-        return productDescription
+    fun getProductName(): Int {
+        return productName
     }
 
     fun getProductIngredients(): Int {
         return productIngredients
     }
 
-    init {
-        checkProductClass(productName, pizzaName)
+    fun getPriceProduct(): Int {
+        return productPrice
     }
+
+    fun getDrinkInfo(): Boolean {
+        return drinkInfoProduct
+    }
+
+    fun getNotPizzaName(): Int? {
+        return notPizzaName
+    }
+
+    fun getProductIDFromOrder(): Int {
+        return productIDFromOrder
+    }
+
+    init {
+        checkProductClass(productName, pizzaName, productID)
+    }
+
 }
