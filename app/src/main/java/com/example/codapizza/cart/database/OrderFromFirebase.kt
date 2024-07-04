c36b8c60d5e4554ca0ba5a3b3a2dfd23d18ea830
Hommaster <ilya.pan.2017@mail.ru>
2024-07-04 23:43:29 +0300

    created class for Firebase with 1 argument

--- app/src/main/java/com/example/codapizza/cart/database/OrderFromFirebase.kt
+++ app/src/main/java/com/example/codapizza/cart/database/OrderFromFirebase.kt
@@ -1,9 +1,23 @@
 package com.example.codapizza.cart.database
 
 import android.os.Parcelable
+import com.example.codapizza.model.Sauce
+import com.example.codapizza.model.Topping
+import com.example.codapizza.model.ToppingPlacement
 import kotlinx.parcelize.Parcelize
+import java.util.Date
+import java.util.UUID
 
 @Parcelize
 data class OrderFromFirebase(
-    val orderList : HashMap<String, HashMap<String, Map<String, HashMap<String, String>>>> = hashMapOf()
+//    var title: String = "",
+//    var description: String = "",
+//    var date: String = "",
+//    var image: Int? = null,
+//    var toppings: Map<String, String> = emptyMap(),
+//    var sauce: Map<String, String> = emptyMap(),
+//    var price: Float = 0f,
+//    var quantity: Int = 1,
+//    var productID: Int = -1
+    val order_list : HashMap<String, HashMap<String, Map<String, HashMap<String, String>>>> = hashMapOf()
 ) : Parcelable
