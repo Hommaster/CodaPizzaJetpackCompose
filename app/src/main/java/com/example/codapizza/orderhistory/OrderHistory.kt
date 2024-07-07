e5fbfe0595b820ce7fb1bdc8ffa119bd7e761ba0
Hommaster <ilya.pan.2017@mail.ru>
2024-07-07 23:55:05 +0300

    4 день я сижу над парсингом информации с Firebase, мне кажется что я близок к решению, но каждый раз прихожу к одной и той же ошибке, используя разные методы, мне даже не в сиду писать на английском

--- app/src/main/java/com/example/codapizza/orderhistory/OrderHistory.kt
+++ app/src/main/java/com/example/codapizza/orderhistory/OrderHistory.kt
@@ -2,8 +2,6 @@ package com.example.codapizza.orderhistory
 
 import android.util.Log
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
@@ -15,7 +13,6 @@ import androidx.compose.material3.CardColors
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
