bdd496d8a4e03256db3fcb14f5b6cffc0acb2d81
Hommaster <ilya.pan.2017@mail.ru>
2024-07-09 00:54:07 +0300

    after 5 days of trial and error, data from Firebase Firestore with the contents of orders was parsed into a view - an order sheet and orders inside the sheet

--- app/src/main/java/com/example/codapizza/orderhistory/OrderHistory.kt
+++ app/src/main/java/com/example/codapizza/orderhistory/OrderHistory.kt
@@ -2,6 +2,8 @@ package com.example.codapizza.orderhistory
 
 import android.util.Log
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
@@ -16,6 +18,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
@@ -44,152 +47,141 @@ fun OrderHistory(
     navController: NavController
 ) {
 
-    val hashMap = remember {
-        hashMapOf<String, OrderFromFirebase>()
-    }
-    val listOrders = remember {
+    val listOrderFromDatabase = remember {
         mutableListOf(listOf<OrderFromFirebase>())
     }
-    val lizGavnoliz: MutableList<List<MutableList<Orders>>> = mutableListOf()
-        val hashMapOfOrderFromFirebase: HashMap<String, MutableList<Orders>> = hashMapOf()
-        val listOrderFromFirebase = mutableListOf<OrderFromFirebase>()
+    val listOrders: MutableList<MutableList<Orders>> = mutableListOf()
 
-        val orderList = remember {
-            mutableListOf<Orders>()
-        }
-        val globalOrderList = remember {
-            mutableListOf<MutableList<Orders>>()
-        }
 
-        val fs = Firebase.firestore
+    val fs = Firebase.firestore
 
-        fs.collection("orders").get().addOnCompleteListener { task ->
-            if (task.isComplete) {
-                task.result.documents.forEach { documentSnapshot ->
-                    listOrders.add(listOf(documentSnapshot.toObject(OrderFromFirebase::class.java)!!))
-//                listOrderFromFirebase.add(documentSnapshot.toObject(OrderFromFirebase::class.java)!!)
-//                hashMap[documentSnapshot.id] = documentSnapshot.toObject(OrderFromFirebase::class.java)!!
-                }
+    fs.collection("orders").get().addOnCompleteListener { task ->
+        if (task.isComplete) {
+            task.result.documents.forEach { documentSnapshot ->
+                listOrderFromDatabase.add(listOf(documentSnapshot.toObject(OrderFromFirebase::class.java)!!))
             }
         }
+    }
 
-        listOrders.forEach { listOrders1 ->
-            orderList.clear()
-            if (listOrders1.isNotEmpty()) {
-                Log.d("list1", "${listOrders1}")
-                listOrders1.forEach { it ->
-                    Log.d("itList2", "$it")
-                    it.order_list.forEach { order ->
-                        Log.d("itList3", "$order")
-                        order.value.forEach { key, value2 ->
-                            val orderOne = Orders()
-                            Log.d("itList4", "$value2")
-                            value2["sauces"]!!.forEach { k->
-                                orderOne.sauce = mapOf(
-                                    Sauce.valueOf(k.key) to k.value.toInt()
-                                )
-                            }
-                            value2["toppings"]!!.forEach { k ->
-                                orderOne.toppings = mapOf(
-                                    Topping.valueOf(k.key) to ToppingPlacement.valueOf(k.value)
-                                )
-                            }
-                            value2["product_info"]!!.forEach { valu ->
-                                when(valu.key) {
-                                    "product_name" -> {
-                                        orderOne.title = valu.value
-                                    }
-                                    "product_price" -> {
-                                        orderOne.price = valu.value.toFloat()
-                                    }
-                                    "product_quantity" -> {
-                                        orderOne.quantity = valu.value.toInt()
-                                    }
-                                    "product_date" -> {
-                                        orderOne.description = valu.value
-                                    }
+    listOrderFromDatabase.forEach { listOrders1 ->
+        if (listOrders1.isNotEmpty()) {
+            listOrders1.forEach { it ->
+                Log.d("itList2", "$it")
+                it.order_list.forEach { order ->
+                    val ordersList: MutableList<Orders> = mutableListOf()
+                    Log.d("itList3", "$order")
+                    order.value.forEach { key, value2 ->
+                        val orderOne = Orders()
+                        Log.d("itList4", "$value2")
+                        value2["sauces"]!!.forEach { k->
+                            orderOne.sauce = mapOf(
+                                Sauce.valueOf(k.key) to k.value.toInt()
+                            )
+                        }
+                        value2["toppings"]!!.forEach { k ->
+                            orderOne.toppings = mapOf(
+                                Topping.valueOf(k.key) to ToppingPlacement.valueOf(k.value)
+                            )
+                        }
+                        value2["product_info"]!!.forEach { valu ->
+                            when(valu.key) {
+                                "product_name" -> {
+                                    orderOne.title = valu.value
+                                }
+                                "product_price" -> {
+                                    orderOne.price = valu.value.toFloat()
+                                }
+                                "product_quantity" -> {
+                                    orderOne.quantity = valu.value.toInt()
+                                }
+                                "product_date" -> {
+                                    orderOne.description = valu.value
                                 }
                             }
-                            orderList.add(orderOne)
-                            Log.d("orderList", "$orderList")
                         }
-                        Log.d("orderList2", "$orderList")
+                        ordersList.add(orderOne)
                     }
+                    listOrders.add(ordersList)
                 }
-                hashMapOfOrderFromFirebase[orderList.hashCode().toString()] = orderList
             }
-//            globalOrderList.add(orderList)
-//            Log.d("globalOrderList1", "$globalOrderList")
-            lizGavnoliz.add(listOf(orderList))
-            Log.d("govna", "$lizGavnoliz")
         }
+    }
 
-        SwipeToDismiss(
-            navController
+    SwipeToDismiss(
+        navController
+    ) {
+        Column(
+            modifier = Modifier
+                .background(colorResource(id = R.color.black))
+                .fillMaxSize(),
         ) {
+            TopAppBarForScreens(
+                textID = R.string.order_history,
+                navController = navController,
+                mainActivityViewModel = MainActivityViewModel(),
+                cartNotEmpty = false
+            )
             Column(
                 modifier = Modifier
-                    .background(colorResource(id = R.color.black))
-                    .fillMaxSize(),
+                    .padding(30.dp, 250.dp)
+                    .align(Alignment.CenterHorizontally)
             ) {
-                TopAppBarForScreens(
-                    textID = R.string.order_history,
-                    navController = navController,
-                    mainActivityViewModel = MainActivityViewModel(),
-                    cartNotEmpty = false
-                )
-                Column(
-                    modifier = Modifier
-                        .padding(30.dp, 250.dp)
-                        .align(Alignment.CenterHorizontally)
+                Card(
+                    shape = RoundedCornerShape(15.dp),
+                    colors = CardColors(
+                        colorResource(id = R.color.orange),
+                        Color.Cyan,
+                        Color.Cyan,
+                        Color.Cyan
+                    )
                 ) {
-                    Card(
-                        shape = RoundedCornerShape(15.dp),
-                        colors = CardColors(
-                            colorResource(id = R.color.orange),
-                            Color.Cyan,
-                            Color.Cyan,
-                            Color.Cyan
-                        )
+                    Text(
+                        modifier = Modifier
+                            .padding(2.dp, 2.dp),
+                        color = colorResource(id = R.color.white),
+                        text = stringResource(id = R.string.order_history_is_empty),
+                        fontStyle = FontStyle.Italic,
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 20.sp,
+                        style = TextStyle(textIndent = TextIndent(15.sp, 30.sp))
+                    )
+                    Row(
+                        modifier = Modifier
+                            .align(Alignment.CenterHorizontally)
                     ) {
-                        Text(
-                            modifier = Modifier
-                                .padding(2.dp, 2.dp),
-                            color = colorResource(id = R.color.white),
-                            text = stringResource(id = R.string.order_history_is_empty),
-                            fontStyle = FontStyle.Italic,
-                            fontWeight = FontWeight.Bold,
-                            fontSize = 20.sp,
-                            style = TextStyle(textIndent = TextIndent(15.sp, 30.sp))
-                        )
-                        Row(
-                            modifier = Modifier
-                                .align(Alignment.CenterHorizontally)
-                        ) {
-                            TextButton(onClick = {
-                                navController.popBackStack("screen_1", false)
-                            }) {
-                                Text(
-                                    color = colorResource(id = R.color.black),
-                                    text = stringResource(id = R.string.back_to_menu),
-                                    fontStyle = FontStyle.Italic,
-                                    fontWeight = FontWeight.Bold,
-                                    fontSize = 20.sp,
-                                )
-                            }
+                        TextButton(onClick = {
+                            navController.popBackStack("screen_1", false)
+                        }) {
+                            Text(
+                                color = colorResource(id = R.color.black),
+                                text = stringResource(id = R.string.back_to_menu),
+                                fontStyle = FontStyle.Italic,
+                                fontWeight = FontWeight.Bold,
+                                fontSize = 20.sp,
+                            )
                         }
-                        LazyColumn {
-                            item {
-                                hashMap.values.forEach { OrderFromFirebase ->
-                                    OrderFromFirebase.order_list.values.forEach { value1 ->
-                                        value1.values
+                    }
+                    LazyColumn {
+                        item {
+                            val middle = listOrders.size/2
+                            listOrders.subList(0, middle).forEach { orderList ->
+                                Box(
+                                    modifier = Modifier
+                                        .border(2.dp, Color.Red, RoundedCornerShape(13.dp))
+                                        .clip(RoundedCornerShape(13.dp))
+                                ) {
+                                    Column {
+                                        orderList.forEach { order ->
+                                            Text(text = order.title)
+                                        }
                                     }
                                 }
-
                             }
+
                         }
                     }
                 }
             }
         }
     }
+}
