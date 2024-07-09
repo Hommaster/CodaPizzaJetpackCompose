4ededd955d23284e84c99ce3a9c9db1a585b495a
Hommaster <ilya.pan.2017@mail.ru>
2024-07-09 15:21:54 +0300

    cleared code

--- app/src/main/java/com/example/codapizza/orderhistory/FilledOrderHistory.kt
+++ app/src/main/java/com/example/codapizza/orderhistory/FilledOrderHistory.kt
@@ -1,7 +1,6 @@
 package com.example.codapizza.orderhistory
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.lazy.LazyColumn
@@ -9,8 +8,6 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.unit.dp
 import com.example.codapizza.R
